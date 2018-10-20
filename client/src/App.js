e3d80d91d80dd4980432dca675a0ea5efa5a2e54
Carl Burger <cybernomad@gmx.de>
2018-10-20 18:24:07 +0200

    added function for pairing

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>CCinder</h1>
+          <button type="button" name="randomizeAttendance" onClick={() => RandomizeAttendance()}>Randomize Attendance</button>
+          <button type="button" name="generatePairings" onClick={() => GeneratePairings()}>Generate Pairings</button>
         </header>
         <main className="App-main">
           <AttendeeList />
@@ -24,22 +26,22 @@ class AttendeeList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      attendees: []
+      users: []
     };
   }
 
   componentDidMount() {
     var self = this;
-    fetch(`api/attendees`, {
+    fetch(`api/users`, {
       accept: "application/json"
     }).then(res => res.json()).then(json => {
       console.log(json);
-      let attendees = [];
+      let users = [];
       json.forEach((attendee,i) => {
-        attendees.push(<li key={i}>{attendee.Vorname} {attendee.Nachname}, {attendee.Team}</li>);
+        users.push(<li key={i}>{attendee.Vorname} {attendee.Nachname}, {attendee.Anwesend}</li>);
       });
       self.setState({
-        attendees: attendees
+        users: users
       });
     });      
   }
@@ -48,9 +50,26 @@ class AttendeeList extends Component {
     console.log(this.state);
     return (
       <ul>
-        {this.state.attendees}
+        {this.state.users}
       </ul>
     );
   }
 }
+
+function RandomizeAttendance() {
+  fetch(`api/randomizeattendance`, {
+    accept: "application/json"
+  }).then(res => res.json()).then(json => {
+    console.log("Randomized: ", json);
+  });   
+}
+
+function GeneratePairings() {
+  fetch(`api/generatepairings?rounds=3`, {
+    accept: "application/json"
+  }).then(res => res.json()).then(json => {
+    console.log("Pairings: ",json);
+  });   
+}
+
 export default App;
--- server.js
+++ server.js
@@ -11,14 +11,67 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("/api/attendees", (req, res) => {
+app.get("/api/users", (req, res) => {
   (async function() {
     let client;
     try {
-      client = await MongoClient.connect(url);
+      client = await MongoClient.connect(url, {useNewUrlParser: true});
       const db = client.db(dbName);
-      const result = await db.collection('attendees').find({}).toArray();
+      const result = await db.collection('users').find({Anwesend: 1}).toArray();
       res.json(result);
+    } catch (err) {
+      console.log(err.stack);
+    }
+  
+    if (client) {
+      client.close();
+    }
+  })();
+});
+
+app.get("/api/randomizeattendance", (req,res) => {
+  (async function() {
+    let client;
+    try {
+      client = await MongoClient.connect(url, {useNewUrlParser: true});
+      const db = client.db(dbName);
+      const result = await db.collection('users').find({}).forEach((doc) => {
+        db.collection('users').updateOne({_id: doc._id}, {$set: {Anwesend: getRandomInt(6)}});
+      });
+      res.json(result);
+    } catch (err) {
+      console.log(err.stack);
+    }
+  
+    if (client) {
+      client.close();
+    }
+  })();
+});
+
+app.get("/api/generatepairings", (req,res) => {
+  const rounds = req.query.rounds || 6;
+  (async function() {
+    let client;
+    try {
+      client = await MongoClient.connect(url, {useNewUrlParser: true});
+      const db = client.db(dbName);
+      const attendees = await db.collection('users').find({Anwesend: 1}).toArray;
+      let possiblePairs = getPairs(attendees);
+      console.log("possible pairs:",possiblePairs);
+      let pairings = {}
+      for (j=0; j++; j<attendees.length) {
+        const pairsWithAttendee = Object.keys(possiblePairs).filter(function(key) {
+          return key.indexOf(attendees[j]) !== -1
+        });
+        for (i=0; i++; i<rounds) {
+          const pick = getRandomInt(pairsWithAttendee.length);
+          const partner = await db.collection('users').findOne({"E-Mail-Adresse": pairsWithAttendee[pick].replace(attendees[j], '')})._id;
+          pairings[attendees[j]][i] = partner;
+          delete possiblePairs[pairsWithAttendee[pick]];
+        }
+      }
+      res.json(pairings);
     } catch (err) {
       console.log(err.stack);
     }
@@ -32,3 +85,25 @@ app.get("/api/attendees", (req, res) => {
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getPairs(list) {
+  pairings = {};
+  for (var i=0; i++; i<list.length) {
+    for (var j=0; j++; j<list.length) {
+      if (i !== j) {
+        pairings[combineSorted([list[i]["E-Mail-Adresse"], list[j]["E-Mail-Adresse"]])] = 1;
+      }
+    }
+  }
+  return pairings;
+}
+
+function combineSorted(a, b) {
+  if(a < b) return a+b;
+  if(a > b) return b+a;
+  return 0;        
+}
